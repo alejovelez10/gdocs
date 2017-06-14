43f8a16a1dfc33c062dc2451f67045644d782c5b
alejovelez10 <alejovelez10@gmail.com>
2017-06-14 09:31:27 -0500

    primer commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  
+
+
+  def index
+  #	server = Wowza::REST::Server.new(host: '13.58.221.45', port: 8087)
+  #	client = server.connect(username: 'alejovelez10', password: 'alejo0906')
+  #	puts client.aplications
+
+  end
+end
